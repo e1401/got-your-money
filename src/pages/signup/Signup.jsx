7895f6d2bef69a7891f7a949b0cc8264d6b4167b
Antonio Hadrovic <hadrovic1@gmail.com>
2022-01-31 09:19:37 +0100

    Add login ad signup form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,34 @@
 import styles from './Signup.module.css';
+import { useState } from 'react';
 
 function Signup() {
-  return <div>Sign-up</div>;
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Signup</h2>
+      <label>
+        <span>Display name:</span>
+        <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
 
 export default Signup;
