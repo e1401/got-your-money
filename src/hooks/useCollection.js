3a36c054677877d6b9e3a02347f283dc26429b30
Antonio Hadrovic <hadrovic1@gmail.com>
2022-02-03 10:12:45 +0100

    Add useCollection hook and list some transactions

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+export const useCollection = (collection) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection);
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
+        //update the state
+        setDocuments(results);
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+        setError('Could not fetch data');
+      }
+    );
+    //unsubscribe from real-time listener on component unmount
+    return () => unsubscribe();
+  }, [collection]);
+  return { documents, error };
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,20 @@
 import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
+
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 import styles from './Home.module.css';
 
 function Home() {
+  const { documents, error } = useCollection('transactions');
   const { user } = useAuthContext();
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
