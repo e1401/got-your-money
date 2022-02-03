4e0c0b8f44024d7114aa7f3d31f939e874729ce9
Antonio Hadrovic <hadrovic1@gmail.com>
2022-02-03 15:19:45 +0100

    Add descending transaction sorting

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -2,18 +2,23 @@ import { useState, useEffect } from 'react';
 import { useRef } from 'react';
 import { projectFirestore } from '../firebase/config';
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _orderBy) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   //infinite loop alert if you don't useRef
   const query = useRef(_query).current;
+  const orderBy = useRef(_orderBy).current;
 
   useEffect(() => {
     let ref = projectFirestore.collection(collection);
 
     if (query) {
       ref = ref.where(...query);
+    }
+
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy);
     }
 
     const unsubscribe = ref.onSnapshot(
@@ -33,6 +38,6 @@ export const useCollection = (collection, _query) => {
     );
     //unsubscribe from real-time listener on component unmount
     return () => unsubscribe();
-  }, [collection, query]);
+  }, [collection, query, orderBy]);
   return { documents, error };
 };
